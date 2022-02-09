0537422895413992e6ef6a12d2c5b106cbb8bb67
Liamr <liamreitsma@icloud.com>
2022-02-09 16:31:56 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const overlay = document.querySelector(".overlay");
+const image = document.querySelector(".image");
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+overlay.style.display = "none";
+
+console.log(window.innerWidth)
+
+if (window.innerWidth > 600) {
+  image.addEventListener("mouseover", async () => {
+    overlay.style.display = "flex";
+    overlay.style.animation = "Overlay .5s ease";
+    await sleep(500);
+  });
+
+  image.addEventListener("mouseleave", async () => {
+    overlay.style.animation = "OverlayOut .5s ease";
+    await sleep(500);
+    overlay.style.display = "none";
+  });
+}
